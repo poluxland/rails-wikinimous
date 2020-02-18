917735c0ea8fe35bba250df412314837d884dcda
Jose Jerez <joseignaciojerez@gmail.com>
2020-02-18 18:01:00 -0300

    defs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+30.times do
+new_article = Article.new(title: Faker::Book.title, content: Faker::Movies::PrincessBride.quote)
+new_article.save
+end
